19f0a2704366a5cd6083e3030ca77862258eadd6
L. F. Pereira <luisfgpereira95@gmail.com>
2021-05-11 11:16:45 +0200

    Add setup of a simple trapped vortex heat case

--- src/fenics_utils/setups/heat.py
+++ src/fenics_utils/setups/heat.py
@@ -68,3 +68,43 @@ def set_heat_eq_2d(dt, n=8):
     u = Function(V, name=var_name)
 
     return u_n, a, L, u, bcs
+
+
+def set_trapped_vortex(mesh, dt, var_name='Temperature'):
+
+    # function space
+    V = FunctionSpace(mesh, 'Lagrange', 1)
+
+    # boundary conditions
+    x_min, x_max = 0., 0.2
+    u_xmin, u_xmax = Constant(300.), Constant(300.)
+
+    def boundary_xmin(x, on_boundary):
+        return on_boundary and near(x[0], x_min)
+
+    def boundary_xmax(x, on_boundary):
+        return on_boundary and near(x[0], x_max)
+
+    bcs = [DirichletBC(V, u_xmin, boundary_xmin),
+           DirichletBC(V, u_xmax, boundary_xmax)]
+
+    # initial condition
+    c = (x_max - x_min) / 2
+    param = u_xmin.values()[0] / (c**2)
+    u_initial = Expression('param * (x[0] - c) * (x[0] - c)',
+                           degree=2, param=param, c=c, name='T')
+
+    u_n = Function(V, name=var_name)
+    u_n.interpolate(u_initial)
+
+    # problem formulation
+    u_h = TrialFunction(V)
+    v = TestFunction(V)
+    f = Constant(0.)
+
+    a = u_h * v * dx + dt * dot(grad(u_h), grad(v)) * dx
+    L = (u_n + dt * f) * v * dx
+
+    u = Function(V, name=var_name)
+
+    return u_n, a, L, u, bcs
