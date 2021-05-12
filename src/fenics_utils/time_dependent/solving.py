041ea9a30994a85351d6778616bda6fa0fa2b976
L. F. Pereira <luisfgpereira95@gmail.com>
2021-05-12 10:37:02 +0200

    Add stop criterion to solve time dependent and add simple stop criteria

--- src/fenics_utils/time_dependent/solving.py
+++ src/fenics_utils/time_dependent/solving.py
@@ -1,30 +1,92 @@
+from abc import ABCMeta
+from abc import abstractmethod
 
+# TODO: merge functions
 
-# TODO: update to receive objects which expression change with time
+def solve_time_dependent(u_n, u, solver, dt, file, timer, stop_criterion):
 
-def solve_time_dependent(u_n, u, solver, dt, num_steps, vtkfile, timer):
-
-    # TODO: return solution
+    # TODO: need to abstract file type
+    # TODO: return solution?
+    # TODO: update objects which expression change with time (user has to tell?)
 
     t = 0
-    vtkfile << (u_n, t)
+    file << (u_n, t)
     timer.start()
 
-    for n in range(num_steps):
+    it = 0
+    while True:
         timer.start_iter()
 
-        # update curent time
+        it += 1
+
+        # update current time
         t += dt
 
         # compute solution
         solver.solve()
 
         # save to vtk file
-        vtkfile << (u, t)
+        file << (u, t)
 
         # update previous solution
         u_n.assign(u)
 
+        # TODO: make timer also count time to check criteria
+        stop = stop_criterion.check(it=it, t=t, u=u)
         timer.stop_iter()
 
+        if stop:
+            break
+
     timer.stop()
+
+
+class Criterion(metaclass=ABCMeta):
+
+    @abstractmethod
+    def check(self, it, t, u):
+        '''
+        Receives number of iteration, time and current solution and provides
+        stop information (True means the criterion was reached).
+        '''
+        pass
+
+
+class StopByTime(Criterion):
+
+    def __init__(self, t_max):
+        self.t_max = t_max
+
+    def check(self, t, **kwargs):
+        if t >= self.t_max:
+            return True
+
+        return False
+
+
+class StopByIterations(Criterion):
+
+    def __init__(self, max_iters):
+        super().__init__()
+        self.max_iters = max_iters
+
+    def check(self, it, **kwargs):
+        if it == self.max_iters:
+            return True
+
+        return False
+
+
+class StopByExtremaDiff(Criterion):
+
+    def __init__(self, min_max_diff, max_iters=1000):
+        self.min_max_diff = min_max_diff
+        self.max_iters = max_iters  # for protection
+
+    def check(self, it, u, **kwargs):
+        u_vec = u.vector()
+        diff = u_vec.max() - u_vec.min()
+        if diff <= self.min_max_diff or it == self.max_iters:
+            return True
+
+        return False
