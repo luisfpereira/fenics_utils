1dc3c21d51b55d02fb38364f79aecf87464633fb
L. F. Pereira <luisfgpereira95@gmail.com>
2021-05-12 12:58:26 +0200

    Refactor heat setup to be more generic

--- src/fenics_utils/setups/heat.py
+++ src/fenics_utils/setups/heat.py
@@ -15,10 +15,60 @@ from dolfin.function.function import Function
 
 from dolfin.fem.dirichletbc import DirichletBC
 
-
 from ufl import dot
 from ufl import dx
 from ufl import grad
+
+
+def set_heat_equal_opposite(mesh, dt, var_name='Temperature', axis=0,
+                            bc_temperature=300., source_value=0.):
+    '''
+    Heat equation problem where two opposite edges are set at the same 
+    temperature and the points in between follow a quadratic variation.
+
+    Notes
+    -----
+    * assumes opposite faces of interest are planar and perpendicular to the 
+    axis.
+    '''
+
+    # function space
+    V = FunctionSpace(mesh, 'Lagrange', 1)
+
+    # boundary conditions
+    mesh_coords = mesh.coordinates()
+    x_min, x_max = mesh_coords[:, axis].min(), mesh_coords[:, axis].max()
+    u_xmin, u_xmax = Constant(bc_temperature), Constant(bc_temperature)
+
+    def boundary_xmin(x, on_boundary):
+        return on_boundary and near(x[axis], x_min)
+
+    def boundary_xmax(x, on_boundary):
+        return on_boundary and near(x[axis], x_max)
+
+    bcs = [DirichletBC(V, u_xmin, boundary_xmin),
+           DirichletBC(V, u_xmax, boundary_xmax)]
+
+    # initial condition
+    c = (x_max - x_min) / 2
+    param = u_xmin.values()[0] / (c**2)
+    u_initial = Expression('param * (x[axis] - c) * (x[axis] - c)',
+                           degree=2, param=param, c=c, name='T', axis=axis)
+
+    u_n = Function(V, name=var_name)
+    u_n.interpolate(u_initial)
+
+    # problem formulation
+    u_h = TrialFunction(V)
+    v = TestFunction(V)
+    f = Constant(source_value)
+
+    a = u_h * v * dx + dt * dot(grad(u_h), grad(v)) * dx
+    L = (u_n + dt * f) * v * dx
+
+    u = Function(V, name=var_name)
+
+    return u_n, a, L, u, bcs
 
 
 def set_heat_eq_2d(dt, n=8):
@@ -31,80 +81,9 @@ def set_heat_eq_2d(dt, n=8):
     # mesh
     mesh = UnitSquareMesh(n, n)
 
-    # function space
-    V = FunctionSpace(mesh, 'P', 1)
-
-    # bcs
-    x_min, x_max = 0., 1.
-    u_xmin, u_xmax = Constant(500.), Constant(500.)
-
-    def boundary_xmin(x, on_boundary):
-        return on_boundary and near(x[0], x_min)
-
-    def boundary_xmax(x, on_boundary):
-        return on_boundary and near(x[0], x_max)
-
-    bcs = [DirichletBC(V, u_xmin, boundary_xmin),
-           DirichletBC(V, u_xmax, boundary_xmax)]
-
-    # initial condition
-    param = u_xmin.values()[0] / (.5**2)
-    c = (x_max - x_min) / 2.
-
-    u_initial = Expression('param * (x[0] - c) * (x[0] - c)',
-                           degree=2, param=param, c=c, name='T')
-
-    u_n = Function(V, name=var_name)
-    u_n.interpolate(u_initial)
-
-    # problem formulation
-    u_h = TrialFunction(V)
-    v = TestFunction(V)
-
-    f = Constant(0.)
-    a = u_h * v * dx + dt * dot(grad(u_h), grad(v)) * dx
-    L = (u_n + dt * f) * v * dx
-
-    u = Function(V, name=var_name)
-
-    return u_n, a, L, u, bcs
+    return set_heat_equal_opposite(mesh, dt, var_name=var_name)
 
 
-def set_trapped_vortex(mesh, dt, var_name='Temperature'):
+def set_trapped_vortex_for_benchmark(mesh, dt, var_name='Temperature'):
 
-    # function space
-    V = FunctionSpace(mesh, 'Lagrange', 1)
-
-    # boundary conditions
-    x_min, x_max = 0., 0.2
-    u_xmin, u_xmax = Constant(300.), Constant(300.)
-
-    def boundary_xmin(x, on_boundary):
-        return on_boundary and near(x[0], x_min)
-
-    def boundary_xmax(x, on_boundary):
-        return on_boundary and near(x[0], x_max)
-
-    bcs = [DirichletBC(V, u_xmin, boundary_xmin),
-           DirichletBC(V, u_xmax, boundary_xmax)]
-
-    # initial condition
-    c = (x_max - x_min) / 2
-    param = u_xmin.values()[0] / (c**2)
-    u_initial = Expression('param * (x[0] - c) * (x[0] - c)',
-                           degree=2, param=param, c=c, name='T')
-
-    u_n = Function(V, name=var_name)
-    u_n.interpolate(u_initial)
-
-    # problem formulation
-    u_h = TrialFunction(V)
-    v = TestFunction(V)
-    f = Constant(0.)
-
-    a = u_h * v * dx + dt * dot(grad(u_h), grad(v)) * dx
-    L = (u_n + dt * f) * v * dx
-
-    u = Function(V, name=var_name)
-
-    return u_n, a, L, u, bcs
+    pass
