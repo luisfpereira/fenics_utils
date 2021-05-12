12bb35743aa0f64f7271f5a3dca0080c8bf92c46
L. F. Pereira <luisfgpereira95@gmail.com>
2021-05-12 17:32:15 +0200

    Add another heat equation example (that will be used for benchmarking)

--- src/fenics_utils/setups/heat.py
+++ src/fenics_utils/setups/heat.py
@@ -20,15 +20,65 @@ from ufl import dx
 from ufl import grad
 
 
+# TODO: add material parameters to all the functions
+
+
+def set_heat_dirichlet_constant(mesh, dt, var_name='Temperature',
+                                bc_temperature=1300., temperature=300.,
+                                source_value=0., axis=1, face='max', tol=1e-4):
+    '''
+    Heat equation problem where an edge/surface is set at a given temperature
+    and everything else at another.
+
+    Parameters
+    ----------
+    axis : int
+        Controls boundary to which Dirichlet bc is applied.
+    face : str
+        Dirichlet bc is applied to the planar and perpendicular face found in
+        the max ('max') or min ('min') coordinate.
+    '''
+
+    # function space
+    V = FunctionSpace(mesh, 'Lagrange', 1)
+
+    # boundary conditions (only upper hole has Dirichlet bc)
+    mesh_coords = mesh.coordinates()
+    x_bc = mesh_coords[:, axis].max() if face == 'max' else mesh_coords[:, axis].min()
+    u_xbc = Constant(bc_temperature)
+
+    def boundary_x(x, on_boundary):
+        return on_boundary and near(x[axis], x_bc, tol)
+
+    bcs = [DirichletBC(V, u_xbc, boundary_x)]
+
+    # initial condition
+    u_initial = Constant(temperature)
+    u_n = Function(V, name=var_name)
+    u_n.interpolate(u_initial)
+
+    # problem formulation
+    u_h = TrialFunction(V)
+    v = TestFunction(V)
+    f = Constant(source_value)
+
+    a = u_h * v * dx + dt * dot(grad(u_h), grad(v)) * dx
+    L = (u_n + dt * f) * v * dx
+
+    u = Function(V, name=var_name)
+
+    return u_n, a, L, u, bcs
+
+
 def set_heat_equal_opposite(mesh, dt, var_name='Temperature', axis=0,
                             bc_temperature=300., source_value=0.):
     '''
-    Heat equation problem where two opposite edges are set at the same 
+    Heat equation problem where two opposite edges/faces are set at the same
     temperature and the points in between follow a quadratic variation.
 
     Notes
     -----
-    * assumes opposite faces of interest are planar and perpendicular to the 
+    * assumes opposite faces of interest are planar and perpendicular to the
     axis.
     '''
 
@@ -82,8 +132,3 @@ def set_heat_eq_2d(dt, n=8):
     mesh = UnitSquareMesh(n, n)
 
     return set_heat_equal_opposite(mesh, dt, var_name=var_name)
-
-
-def set_trapped_vortex_for_benchmark(mesh, dt, var_name='Temperature'):
-
-    pass
