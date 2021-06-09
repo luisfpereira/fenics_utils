c81b981c610c8639a7d84ea73dbcfdd1b9d229fe
L. F. Pereira <luisfgpereira95@gmail.com>
2021-06-09 12:04:37 +0200

    Add small utils to get limit values of mesh coordinates

--- src/fenics_utils/mesh.py
+++ src/fenics_utils/mesh.py
@@ -53,3 +53,20 @@ def create_unit_hypercube(n):
 
     mesh_obj = [UnitIntervalMesh, UnitSquareMesh, UnitCubeMesh]
     return mesh_obj[len(n) - 1](*n)
+
+
+def get_mesh_axes_lims(mesh):
+    mesh_coords = mesh.coordinates()
+    return mesh_coords.min(axis=0), mesh_coords.max(axis=0)
+
+
+def get_mesh_axis_lims(mesh, axis=0):
+    mesh_coords = mesh.coordinates()
+    x_min, x_max = mesh_coords[:, axis].min(), mesh_coords[:, axis].max()
+
+    return x_min, x_max
+
+
+def get_mesh_axis_lim(mesh, axis=0, right=True):
+    mesh_coords = mesh.coordinates()
+    return mesh_coords[:, axis].max() if right else mesh_coords[:, axis].min()
