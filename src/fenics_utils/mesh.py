ac79d66032f41386ad26004531e6ca6c2132ec39
L. F. Pereira <luisfgpereira95@gmail.com>
2021-06-11 13:09:13 +0200

    Move utils related with cerfacs mesh to its own module

--- src/fenics_utils/mesh.py
+++ src/fenics_utils/mesh.py
@@ -1,44 +1,7 @@
-import os
 
-from pyhip.commands.readers import read_hdf5_mesh
-from pyhip.commands.writers import write_gmsh
-from pyhip.commands.operations import hip_exit
-
-import meshio
-
-from dolfin.cpp.mesh import Mesh
 from dolfin.cpp.generation import UnitIntervalMesh
 from dolfin.cpp.generation import UnitSquareMesh
 from dolfin.cpp.generation import UnitCubeMesh
-
-
-def get_dolfin_mesh_from_hdf5(mesh_filename):
-
-    # convert mesh
-    dolfin_filename = convert_hdf5_to_dolfin_mesh(mesh_filename)
-
-    # load dolfin mesh
-    return Mesh(dolfin_filename)
-
-
-def convert_hdf5_to_dolfin_mesh(mesh_filename):
-
-    mesh_dirname = os.path.dirname(mesh_filename)
-    filename = os.path.basename(mesh_filename).split('.')[0]
-
-    # create gmsh mesh
-    gmsh_filename = os.path.join(mesh_dirname, filename)
-    read_hdf5_mesh(mesh_filename)
-    write_gmsh(gmsh_filename)
-    hip_exit()
-
-    # create dolfin mesh
-    dolfin_filename = os.path.join(mesh_dirname,
-                                   f'{filename}.xml')
-    meshio_mesh = meshio.Mesh.read(f'{gmsh_filename}.mesh.msh')
-    meshio_mesh.write(dolfin_filename)
-
-    return dolfin_filename
 
 
 def create_unit_hypercube(n):
