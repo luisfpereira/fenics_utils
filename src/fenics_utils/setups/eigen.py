eb27e01cda37deaaf73afc86fc47b3531c538cf3
L. F. Pereira <luisfgpereira95@gmail.com>
2021-06-22 13:45:39 +0200

    Abstract eigen setup and add new case (all fixed)

--- src/fenics_utils/setups/eigen.py
+++ src/fenics_utils/setups/eigen.py
@@ -3,23 +3,47 @@ test and develop code'''
 
 from dolfin.cpp.la import PETScMatrix
 from dolfin.fem.assembling import SystemAssembler
+from dolfin.fem.assembling import assemble
 
 from fenics_utils.mesh import create_unit_hypercube
+from fenics_utils.bcs import set_dirichlet_bcs_lims_all
 from fenics_utils.formulation.eigen import formulate_laplacian
 
 
 def set_free_unit_hypercube(n, V=None):
-
     mesh = create_unit_hypercube(n)
 
+    return set_generic(mesh, V, bcs_fnc=None, value=0)
+
+
+def set_fixed_unit_hypercube(n, V=None, diag_value=1e6):
+    mesh = create_unit_hypercube(n)
+
+    return set_generic(mesh, V, diag_value, set_dirichlet_bcs_lims_all, value=0.)
+
+
+def set_generic(mesh, V=None, diag_value=1e6, bcs_fnc=None, **kwargs):
+    '''
+    Args:
+        bcs_fnc (callable): first argument must be V.
+        kwargs: passed to bcs_fnc.
+    '''
     V, a, b, dummy = formulate_laplacian(mesh, V)
-    A, B = _assemble_eigen(a, b, dummy)
+
+    # bcs
+    bcs = bcs_fnc(V, **kwargs) if callable(bcs_fnc) else None
+
+    # assemble
+    A, B = _assemble_eigen(a, b, dummy, bcs, diag_value)
 
     return V, A, B
 
 
-def _assemble_eigen(a, b, dummy, bcs=None):
-    A = _assemble_system(a, dummy, bcs)
+def _assemble_eigen(a, b, dummy, bcs=None, diag_value=1e6):
+    if bcs is None:
+        A = _assemble_system(a, dummy)
+    else:
+        A = _assemble_system_control_diag(a, dummy, bcs, diag_value)
     B = _assemble_system(b, dummy, bcs)
 
     return A, B
@@ -29,5 +53,17 @@ def _assemble_system(a, L, bcs=None):
     asm = SystemAssembler(a, L, bcs)
     A = PETScMatrix()
     asm.assemble(A)
+
+    return A
+
+
+def _assemble_system_control_diag(a, dummy, bcs, diag_value):
+    A = PETScMatrix()
+    assemble(a, tensor=A)
+    dummy_vec = assemble(dummy)
+
+    for bc in bcs:
+        bc.zero(A)
+        bc.zero_columns(A, dummy_vec, diag_value)
 
     return A
